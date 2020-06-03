458c7a5d5d7be8333e92494aa6a9af4259764a52
Luis Gerardo <luisgpr99@gmail.com>
2020-06-03 17:10:33 -0600

    Compile script now checks the platform to build the executable. Flex doesn't work on Windows, but using another program called win_flex does work

--- compile.sh
+++ compile.sh
@@ -1,4 +1,17 @@
-flex lexer/lexer.l 
-g++ -g -std=c++17 parser/parser.h parser/parser.cpp *.h *.cpp 
+# flex lexer/lexer.l #IF ON WINDOWS, USE WIN_FLEX
+# g++ -g -std=c++17 parser/parser.h parser/parser.cpp *.h *.cpp 
+# rm *.gch
+# rm parser/parser.h.gch
+
+echo "Checking if platform is Linux, if on Linux, use flex, if not, use win_flex, to generate the lexer"
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+  echo "Platform is Linux"
+  flex lexer/lexer.l 
+else
+  echo "Platform is Windows"
+  win_flex lexer/lexer.l 
+fi
+
+g++ -g -std=c++17 parser/parser.h parser/parser.cpp *.h *.cpp -o l_assembler
 rm *.gch
 rm parser/parser.h.gch
